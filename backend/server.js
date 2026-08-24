c41545d0618c6c3eed1c1fe3318de08a8541b676
Umega Eshan <umegaeshan@gmail.com>
2026-08-24 14:51:08 +0530

    Add MongoDB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
