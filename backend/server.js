8f0f76c7fa5219c45f81917d20e8733412617af8
Umega Eshan <umegaeshan@gmail.com>
2026-08-27 12:08:36 +0530

    const routerfeat: initialize backend with Express, CORS, and dotenv setup
    - Created package.json with necessary dependencies and scripts
    - Added server.js to set up Express server with CORS and JSON parsing
    - Configured environment variables using dotenv
    - Implemented a basic route to confirm the API is running

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
+import userRoute from './routes/userRoute.js'
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users',userRoute);
 
 app.get('/',(req,res) =>{
     res.send("GrR E-Commerce API is running...")
